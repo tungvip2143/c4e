06eeda2d74ec0bc696cf94867532befae42b03ed
VietTung <tungvip2143@gmail.com>
2022-05-31 22:13:32 +0700

    c4e

--- baiktra/bai1/main.js
+++ baiktra/bai1/main.js
@@ -1,3 +1,17 @@
-let a = prompt('nhap so a')
-let b = prompt('nhap so b')
-let i = a < i < b
+const a = parseInt(prompt('Nhap so a: '));
+const b = parseInt(prompt('Nhap so b: '));
+
+for (i = a; i <= b; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++)
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+
+
+    if (i > 1 && flag == 0) {
+        console.log(i);
+    }
+}
